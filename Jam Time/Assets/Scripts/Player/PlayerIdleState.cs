ddeb33cae381e999c970b94ca28f45d628f2188c
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2024-03-07 13:17:09 -0700

    Fixed Grounded Logic
    Needed to change an or to an and

--- Jam Time/Assets/Scripts/Player/PlayerIdleState.cs
+++ Jam Time/Assets/Scripts/Player/PlayerIdleState.cs
@@ -23,14 +23,14 @@ public class PlayerIdleState : PlayerBaseState
 
         // cast a ray to check if we are on the ground and pressing space. So switch to jump state
 
-        Vector2 leftRay = new Vector2(player.transform.position.x - 0.4f, player.transform.position.y - 0.55f);
-        Vector2 rightRay = new Vector2(player.transform.position.x + 0.4f, player.transform.position.y - 0.55f);
+        Vector2 leftRay = new Vector2(player.transform.position.x - 0.5f, player.transform.position.y - 0.55f);
+        Vector2 rightRay = new Vector2(player.transform.position.x + 0.5f, player.transform.position.y - 0.55f);
 
         if (Input.GetKeyDown(KeyCode.Space) && (Physics2D.Raycast(leftRay, Vector2.down, 0.5f) || Physics2D.Raycast(rightRay, Vector2.down, 0.5f)))
             player.SwitchState(player.jumpState);
 
         // switch to fall when not on ground
-        if (!Physics2D.Raycast(leftRay, Vector2.down, 0.5f) || !Physics2D.Raycast(rightRay, Vector2.down, 0.5f))
+        if (!Physics2D.Raycast(leftRay, Vector2.down, 0.5f) && !Physics2D.Raycast(rightRay, Vector2.down, 0.5f))
             player.SwitchState(player.fallState);
 
 
--- Jam Time/Assets/Scripts/Player/PlayerWalkState.cs
+++ Jam Time/Assets/Scripts/Player/PlayerWalkState.cs
@@ -22,8 +22,8 @@ public class PlayerWalkState : PlayerBaseState
 
     public override void UpdateState(PlayerStateManager player)
     {
-        Vector2 leftRay = new Vector2(player.transform.position.x - 0.4f, player.transform.position.y - 0.55f);
-        Vector2 rightRay = new Vector2(player.transform.position.x + 0.4f, player.transform.position.y - 0.55f);
+        Vector2 leftRay = new Vector2(player.transform.position.x - 0.5f, player.transform.position.y - 0.55f);
+        Vector2 rightRay = new Vector2(player.transform.position.x + 0.5f, player.transform.position.y - 0.55f);
 
         // switch to jump when pressing space and on the ground. use ray cast to check if on ground.
         if (Input.GetKeyDown(KeyCode.Space) && (Physics2D.Raycast(leftRay, Vector2.down, 0.5f) || Physics2D.Raycast(rightRay, Vector2.down, 0.5f)))
@@ -34,7 +34,7 @@ public class PlayerWalkState : PlayerBaseState
 
 
         // switch to fall when not on the ground
-        if (!Physics2D.Raycast(leftRay, Vector2.down, 0.5f) || !Physics2D.Raycast(rightRay, Vector2.down, 0.5f))
+        if (!Physics2D.Raycast(leftRay, Vector2.down, 0.5f) && !Physics2D.Raycast(rightRay, Vector2.down, 0.5f))
             player.SwitchState(player.fallState);
 
 
